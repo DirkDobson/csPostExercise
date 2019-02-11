24b60e8ffc76c40cf28fab5e6f635e95390488f3
Dirk Dobson <dirkvancouver@live.com>
2019-02-10 22:00:01 -0700

    main

--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ namespace StackOverFlowPost
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Would you like to write a post y or n");
+            string input = Console.ReadLine();
         }
     }
 }
