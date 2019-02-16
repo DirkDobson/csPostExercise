08a86b0ba34333fadcbd6c7ac927718f092a6919
Dirk Dobson <dirkvancouver@live.com>
2019-02-15 20:05:42 -0700

    if statment

--- Program.cs
+++ Program.cs
@@ -8,6 +8,14 @@ namespace StackOverFlowPost
         {
             Console.WriteLine("Would you like to write a post y or n");
             string input = Console.ReadLine();
+            if (input == "y")
+            {
+
+            }
+            else
+            {
+
+            }
 
             System.Console.WriteLine("Do you want to like, dislike to the or neither l, d, or n");
             string like = Console.ReadLine();
