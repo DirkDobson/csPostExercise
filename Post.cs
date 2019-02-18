908538831e67c3bf04f02dd598e57bdccbc1b1c7
Dirk Dobson <dirkvancouver@live.com>
2019-02-17 21:13:43 -0700

    discription and date

--- Post.cs
+++ Post.cs
@@ -4,6 +4,9 @@ namespace StackOverFlowPost
 {
   public class Post
   {
+    public DateTime  postCreated = DateTime.Now;
+
+    public string discription = "";
     public string body = ""; 
     public byte likes = 0;
     public byte dislikes = 0;
